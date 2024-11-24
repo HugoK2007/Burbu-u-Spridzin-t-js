da56612aa3d30211a92f7dda24b08ec436f9e413
Hugo Kocers <101929960+HugoK2007@users.noreply.github.com>
2024-11-24 20:58:36 +0200

    Update main.py

--- Burbulu_Spr/main.py
+++ Burbulu_Spr/main.py
@@ -24,6 +24,32 @@ def pārvietot_kuģi(notikums):
         a.move(kuģa_id2, 0, -KUĢA_ĀTR)
     elif notikums.keysym == 'Down':
         a.move(kuģa_id, 0, KUĢA_ĀTR)
-        a.move(kuģa_id2, 0, )
-    
+        a.move(kuģa_id2, 0, KUĢA_ĀTR)
+    elif notikums.keysym == 'Left':
+        a.move(kuģa_id, -KUĢA_ĀTR, 0)
+        a.move(kuģa_id2, -KUĢA_ĀTR, 0)
+    elif notikums.keysym == 'Right':
+        a.move(kuģa_id, KUĢA_ĀTR, 0)
+        a.move(kuģa_id2, KUĢA_ĀTR, 0)
+a.bind_all('<Key>', pārvietot_kuģi)
+
+# 4. 
+from random import randint
+burb_id = list()
+burb_r = list()
+burb_ātrums = list()
+MIN_BURB_R = 10
+MAX_BURB_R = 30
+MAX_BURB_ĀTR = 10
+ATSTARPE = 100
+def izveidot_burbuli():
+    x = PLATUMS + ATSTARPE
+    y = randint(0, GARUMS)
+    r = randint(MIN_BURB_R, MAX_BURB_R)
+    id1 = a.create_oval(x - r, y - r, x + r, y + r, outline='white')
+    burb_id.append(id1)
+    burb_r.append(r)
+    burb_ātrums.append(randint(1, MAX_BURB_ĀTR))
+
+# 5.
 
